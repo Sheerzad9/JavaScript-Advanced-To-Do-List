f33721485b0992e60c80eee59d96d3173430b454
Ali Sheerzad <ali.sheerzad9@gmail.com>
2021-10-11 21:31:13 +0300

    Added some functions

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+const container = document.querySelector(".container")
+const startBtn = document.querySelector(".testButton")
+
+startBtn.addEventListener('click', function(){
+    container.innerHTML = ''
+    // Creating the header
+    const newDiv = document.createElement('div')
+    newDiv.className = "header";
+    //Creating some h2 text inside header
+    const newH2El = document.createElement('h2')
+    const headerText = document.createTextNode('To Do List')
+    newH2El.appendChild(headerText)
+    newDiv.appendChild(newH2El)
+    console.log(newDiv);
+    // Creating input
+    const newInput = document.createElement('input')
+    newInput.type = 'text'
+    newInput.className = 'myInput'
+    newInput.placeholder = 'Title...'
+
+    // Adding new element to container
+    container.appendChild(newDiv);
+    container.appendChild(newInput)
+
+})
+
 console.log("testi");
