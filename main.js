d891a0fb8e58efc8bdec24198ccdb296bf27de81
Ali Sheerzad <ali.sheerzad9@gmail.com>
2021-10-12 20:34:36 +0300

    Added loader and few buttons

--- main.js
+++ main.js
@@ -1,27 +1,41 @@
-const container = document.querySelector(".container")
-const startBtn = document.querySelector(".testButton")
+const container = document.querySelector(".container");
+const startBtn = document.querySelector(".testButton");
+const loader = document.querySelector(".loader");
 
-startBtn.addEventListener('click', function(){
-    container.innerHTML = ''
-    // Creating the header
-    const newDiv = document.createElement('div')
-    newDiv.className = "header";
-    //Creating some h2 text inside header
-    const newH2El = document.createElement('h2')
-    const headerText = document.createTextNode('To Do List')
-    newH2El.appendChild(headerText)
-    newDiv.appendChild(newH2El)
-    console.log(newDiv);
-    // Creating input
-    const newInput = document.createElement('input')
-    newInput.type = 'text'
-    newInput.className = 'myInput'
-    newInput.placeholder = 'Title...'
+startBtn.addEventListener("click", function () {
+  startBtn.style.display = "none";
+  loader.style.opacity = "1";
+  setTimeout("createNewContent()", 3000);
+});
 
-    // Adding new element to container
-    container.appendChild(newDiv);
-    container.appendChild(newInput)
+const createNewContent = function () {
+  // container.innerHTML = "";
+  container.remove();
+  // Creating the header
+  const newDiv = document.createElement("div");
+  newDiv.className = "header";
+  //Creating some h2 text inside header
+  const newH2El = document.createElement("h2");
+  const headerText = document.createTextNode("What should we do today");
+  newH2El.appendChild(headerText);
+  newDiv.appendChild(newH2El);
+  console.log(newDiv);
+  // Creating input
+  const newInput = document.createElement("input");
+  newInput.type = "text";
+  newInput.className = "input";
+  newInput.placeholder = "What should we do...";
+  newDiv.appendChild(newInput);
+  // Creating span element to click
+  const span = document.createElement("span");
+  const spanText = document.createTextNode("Add");
+  span.appendChild(spanText);
+  span.className = "addBtn";
+  newDiv.appendChild(span);
+  // Adding new element
+  document.body.appendChild(newDiv);
 
-})
+  // Creating list container
+};
 
 console.log("testi");
